f614e1767358562d5f639e16f1af62f3f4691660
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2022-06-15 16:22:18 +0200

    CSS nav bar in mobile

--- Js/script.js
+++ Js/script.js
@@ -1,9 +1,13 @@
 // nav mobile
 //burger event listener
 const button = document.querySelector(".nav-burger")
+const navList=document.querySelectorAll(".nav-list .nav-item")
+
 
 button.addEventListener('click', function (event) {
-    event.stopPropagation()
+    for (const navItem of navList) {
+    console.log(navItem.innerText)
+}event.stopPropagation()
 
 }
 )
