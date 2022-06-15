46d3ac5eb7963b51ebccf2280c9c1255fcc4da55
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2022-06-15 16:45:00 +0200

    nav burger js

--- Js/script.js
+++ Js/script.js
@@ -1,13 +1,2 @@
 // nav mobile
 //burger event listener
-const button = document.querySelector(".nav-burger")
-const navList=document.querySelectorAll(".nav-list .nav-item")
-
-
-button.addEventListener('click', function (event) {
-    for (const navItem of navList) {
-    console.log(navItem.innerText)
-}event.stopPropagation()
-
-}
-)
