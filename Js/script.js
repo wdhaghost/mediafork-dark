5157a7fc42579fbcf189ffe117ae511271af8a15
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2022-06-16 08:12:54 +0200

    js nav bar working

--- Js/script.js
+++ Js/script.js
@@ -1,2 +1,21 @@
 // nav mobile
 //burger event listener
+const burger = document.querySelector(".nav-burger")
+const arrow = document.querySelector(".nav-arrow")
+const navList=document.querySelector(".nav-list")
+
+
+burger.addEventListener('click', function (event) {
+  navList.classList.toggle("active")
+  arrow.classList.toggle("active")
+  burger.classList.toggle("desac")
+  document.body.classList.toggle("active")
+}
+)
+arrow.addEventListener('click', function (event) {
+    navList.classList.toggle("active")
+    arrow.classList.toggle("active")
+    burger.classList.toggle("desac")
+    document.body.classList.toggle("active")
+  }
+  )
