7c525a4bebdc7f63d4256bbeff1e92a39284fd69
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2022-06-16 14:24:23 +0200

    working nav bar js and CSS

--- Js/script.js
+++ Js/script.js
@@ -3,7 +3,7 @@
 const burger = document.querySelector(".nav-burger")
 const arrow = document.querySelector(".nav-arrow")
 const navList=document.querySelector(".nav-list")
-
+const navLink=document.querySelectorAll(".nav-link")
 
 burger.addEventListener('click', function (event) {
   navList.classList.toggle("active")
@@ -19,3 +19,15 @@ arrow.addEventListener('click', function (event) {
     document.body.classList.toggle("active")
   }
   )
+for (const link of navLink) {
+  link.addEventListener('click', function (event) {
+    navList.classList.remove("active")
+    arrow.classList.remove("active")
+    burger.classList.remove("desac")
+    document.body.classList.remove("active")
+  }
+  )
+  
+}
+
+  
