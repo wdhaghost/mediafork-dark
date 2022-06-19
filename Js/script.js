37b3df60f87e3ae4b88710d114eabe2ef4497d92
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2022-06-19 20:25:37 +0200

    modal feature working

--- Js/script.js
+++ Js/script.js
@@ -35,9 +35,28 @@ for (const link of navLink) {
 // modal service
 const cards=document.querySelectorAll(".card")
 const cardModal=document.getElementById("card-modal")
-for (const card of cards) {
- card.addEventListener('click',function(event){
-  cardModal.classList.add("active")
+const cardModalTitle=document.getElementById("card-modal-title")
+const cardModalTxt=document.getElementById("card-modal-txt")
+const cardModalImg=document.getElementById("card-modal-img")
+const cardModalContent=document.querySelector(".modal-content")
+
+function displayModal(){
+  cardModal.classList.toggle("active")
+  cardModalTitle.innerText=this.dataset.title
+  cardModalTxt.innerHTML=this.dataset.description
+  cardModalImg.src=this.dataset.img
   
-}) 
 }
+
+cardModalContent.addEventListener("click",function(event){
+ cardModal.classList.toggle("active")
+ event.stopPropagation() 
+})
+
+for (const card of cards) {
+ card.addEventListener("click",displayModal)
+  
+}
+
+
+
